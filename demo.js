0bd5988a32d8d2eaf2a6ec205ff181ca2ee721a2
Gilmore Davidson <gilmoreorless@gmail.com>
2011-02-01 07:58:31 +1100

    Moved index to demo and extracted JS

---
+++ demo.js
@@ -0,0 +1,76 @@
+// can be overwritten in console
+var opts = {
+	includeSelf: false,
+	checkX: true,
+	checkY: true
+};
+
+$(function () {
+	function rand(min, max) {
+		return Math.round(Math.random() * max + min);
+	}
+
+	// Setup random elements
+	var elemCount = 10,
+		minSize = 25,
+		maxSize = 150,
+		$container = $('#container'),
+		contWidth = $container.width(),
+		contHeight = $container.height(),
+		size, x, y, $blocks;
+	while (elemCount--) {
+		size = rand(minSize, maxSize);
+		x = rand(0, contWidth - size);
+		y = rand(0, contHeight - size);
+		$('<div class="block" />').css({
+			width: size,
+			height: size,
+			left: x,
+			top: y
+		}).appendTo($container);
+	}
+	$blocks = $('.block');
+
+	// Demo for $.nearest
+	//*
+	$(document).mousemove(function (e) {
+		var x = e.pageX,
+			y = e.pageY,
+			point = $.extend({x: x, y: y}, opts);
+		$blocks.removeClass('active1 active2 active3').nearest(point).addClass('active1');
+		$.nearest(point, $blocks).addClass('active2');
+		$.furthest(point, $blocks).addClass('active3');
+	});
+
+	// Demo for $.fn.nearest
+	$blocks.click(function () {
+		var $this = $(this);
+		$blocks.removeClass('highlight lowlight').text('');
+		$this.addClass('highlight').text('CLICKED');
+		$this.nearest($blocks, opts).addClass('highlight').text('nearest');
+		$this.furthest($blocks, opts).addClass('lowlight').text('furthest');
+	});
+	//*/
+
+	// Test for chaining
+	/*
+	$('<div/>').css({
+		width:1,
+		height:1,
+		backgroundColor:'#900',
+		position:'absolute',
+		left:500,
+		top:100
+	}).appendTo('body');
+	$blocks
+		.css('backgroundColor','#066')
+		.filter(':odd')
+			.css('backgroundColor','#0FF')
+			.nearest({x:500, y:100})
+				.text('Nearest')
+				.end()
+			.css('color','#F90')
+			.end()
+		.css('opacity', 0.5);
+	//*/
+});
