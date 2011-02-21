3cac0856abf72740fddd35aa0b514abc423286bc
Gilmore Davidson <gilmoreorless@gmail.com>
2011-02-22 08:22:19 +1100

    Renamed checkX/checkY to checkHoriz/checkVert and made script match up with documentation

--- jquery.nearest.js
+++ jquery.nearest.js
@@ -38,8 +38,8 @@
 		var $all = $(selector),
 			filtered = [],
 			furthest = !!options.furthest,
-			checkX = !!options.checkX,
-			checkY = !!options.checkY,
+			checkX = !!options.checkHoriz,
+			checkY = !!options.checkVert,
 			compDist = furthest ? 0 : Infinity,
 			point1x = parseInt(options.x, 10) || 0,
 			point1y = parseInt(options.y, 10) || 0,
@@ -105,10 +105,10 @@
 			y: 0, // Y position of top left corner of point/region
 			w: 0, // Width of region
 			h: 0, // Height of region
-			furthest: name == 'furthest', // Find max distance (true) or min distance (false)
+			furthest:    name == 'furthest', // Find max distance (true) or min distance (false)
 			includeSelf: false, // Include 'this' in search results (t/f) - only applies to $(elem).func(selector) syntax
-			checkX: name != 'touching', // Check variations in X axis (t/f)
-			checkY: name != 'touching'  // Check variations in Y axis (t/f)
+			checkHoriz:  name != 'touching', // Check variations in X axis (t/f)
+			checkVert:   name != 'touching'  // Check variations in Y axis (t/f)
 		};
 
 		/**
@@ -119,13 +119,16 @@
 		 * @param hash point Co-ordinates for the point or region to measure from
 		 *                   "x" and "y" keys are required, "w" and "h" keys are optional
 		 * @param mixed selector Any valid jQuery selector that provides elements to filter
+		 * @param hash options (optional) Extra filtering options
+		 *                     Not technically needed as the options could go on the point object,
+		 *                     but it's good to have a consistent API
 		 * @return jQuery object containing matching elements in selector
 		 */
-		$[name] = function (point, selector) {
+		$[name] = function (point, selector, options) {
 			if (!point || point.x === undefined || point.y === undefined) {
 				return $([]);
 			}
-			var opts = $.extend({}, defaults, point);
+			var opts = $.extend({}, defaults, point, options || {});
 			return $(nearest(selector, opts));
 		};
 
@@ -151,7 +154,7 @@
 		$.fn[name] = function (selector, options) {
 			var opts;
 			if (selector && $.isPlainObject(selector)) {
-				opts = $.extend({}, defaults, selector);
+				opts = $.extend({}, defaults, selector, options || {});
 				return this.pushStack(nearest(this, opts));
 			}
 			var offset = this.offset(),
