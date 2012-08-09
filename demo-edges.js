adf8844eb12ead98cee1b097a90b2ceb7a6d11c7
Gilmore Davidson <gilmoreorless@gmail.com>
2012-08-10 09:12:46 +1000

    Adding dragging of corner handles to resize container

--- demo-edges.js
+++ demo-edges.js
@@ -16,16 +16,76 @@ $(function () {
 		footerHeight = $footer.outerHeight(),
 		corners = ['tl', 'tr', 'br', 'bl'],
 		edgeBorders = {t: 'top', b: 'bottom', l: 'left', r: 'right'},
-		size, $blocks, $corners;
+		edgeLimits = {},
+		size, $blocks, $corners, curDrag;
+
+	// Prevent selecting text while resizing the container
+	function noSelect(e) {
+		if (curDrag) {
+			e.preventDefault();
+		}
+	}
+
+	// Basic handler for any mouse events on drag handles
+	function handleCornerMouseEvents(e) {
+		if (e.type === 'mousedown') {
+			curDrag = {
+				elem: this,
+				corner: $(this).data('corner'),
+				x: e.pageX,
+				y: e.pageY
+			};
+			$(document).on('mousemove', handleCornerMouseEvents);
+			return;
+		}
+		if (e.type === 'mouseup') {
+			if (curDrag && curDrag.elem === this) {
+				curDrag = null;
+				$(document).off('mousemove', handleCornerMouseEvents);
+			}
+			return;
+		}
+		if (e.type === 'mousemove') {
+			if (curDrag) {
+				var dx = e.pageX - curDrag.x,
+					dy = e.pageY - curDrag.y;
+
+				moveContainerCorner(curDrag.corner, dx, dy);
+				curDrag.x = e.pageX;
+				curDrag.y = e.pageY;
+			}
+		}
+	}
+
+	// Move a corner of the container by a certain amount
+	// x/y are relative, not absolute
+	function moveContainerCorner(corner, x, y) {
+		var edgeY = edgeBorders[corner[0]],
+			edgeX = edgeBorders[corner[1]];
+		if (edgeX === 'right') {
+			x = -x;
+		}
+		if (edgeY === 'bottom') {
+			y = -y;
+		}
+		$container.css(edgeX, function (i, val) {
+			return (parseFloat(val) || 0) + x;
+		});
+		$container.css(edgeY, function (i, val) {
+			return (parseFloat(val) || 0) + y;
+		});
+	}
 
 	// Setup corner handles
 	$.each(corners, function (i, corner) {
-		corner = corner.split('');
+		var c = corner.split('');
 		$('<div/>', {
-			'class': ['corner', edgeBorders[corner[0]], edgeBorders[corner[1]]].join(' ')
-		}).appendTo($container);
+			'class': ['corner', edgeBorders[c[0]], edgeBorders[c[1]]].join(' ')
+		}).data('corner', c).appendTo($container);
 	});
 	$corners = $container.find('.corner');
+	$corners.on('mousedown mouseup', handleCornerMouseEvents);
+	$(document).on('selectstart', noSelect);
 
 	// Setup random elements
 	while (elemCount--) {
@@ -56,7 +116,8 @@ $(function () {
 			border = [edgeBorders[dims[0]], edgeBorders[dims[1]]];
 			$corners.filter('.' + border.join('.')).css('background-color', '#F00');
 		} else {
-
+			// TODO: Show middle point with sliders
 		}
+
 	});
 });
